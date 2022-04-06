a1d86a34ef55a649d08985de1c5f5b0bddba4427
jannpey <hfw6915@autuni.ac.nz>
2022-04-06 16:39:09 +0800

    created 404 page

--- frontend/src/pages/404.js
+++ frontend/src/pages/404.js
@@ -4,7 +4,7 @@ class NotFoundPage extends Component {
     return (
     <div>
         <h2> Error 404.  This page does not exist</h2>
-        <p>Select from the menu</p>
+        <p>Select from the menu.</p>
         </div>
     );
 }
