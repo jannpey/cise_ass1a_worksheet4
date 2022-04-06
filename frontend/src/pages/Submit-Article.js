3b1c025927ff235743012a8eda4da78ce492ccbd
jannpey <hfw6915@autuni.ac.nz>
2022-04-06 16:41:41 +0800

    created Submit Article

--- frontend/src/pages/Submit-Article.js
+++ frontend/src/pages/Submit-Article.js
@@ -6,7 +6,7 @@ class SubmitArticle  extends Component {
     return (
     <div>
         <h2>Submit Article</h2>
-        <p>This will be a form to allow submitters to submit an article for possible inclusion in our SEPER repo.</p>
+        <p>This will be a form to allow submitters to submit an article for possible inclusion in our SEPER repo</p>
         <p>Also the display table of evidence levels for different claims will show here for the SE practice selected</p>
         <p>Also the display table of evidence levels for different claims will show here for the SE practice selected</p>
         <SubmissionForm></SubmissionForm>
